f6f5e6ea8bc4f6ee8a98fa66ec50a396a1feed66
Alex <>
2023-01-19 19:22:51 +0100

    add vue2 and vue3 for comparisson

---
+++ vue2-vite-project/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+
+    <button @click="count++">count is: {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test hot module replacement.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue2-vite-project/src/main.js
@@ -0,0 +1,13 @@
+// import { createApp } from 'vue'
+// import App from './App.vue'
+
+// import './assets/main.css'
+
+// createApp(App).mount('#app')
+
+import Vue from 'vue';
+import App from './App.vue';
+
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
